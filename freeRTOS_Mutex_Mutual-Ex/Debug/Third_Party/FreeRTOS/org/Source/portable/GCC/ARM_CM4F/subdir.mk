ee9ff2c1774a31ba556818efce62bcef5a0e6f11
sanujkul <sanujkul@gmail.com>
2020-03-31 19:28:03 +0530

    Achieving mutual exclusion using Mutex

---
+++ freeRTOS_Mutex_Mutual-Ex/Debug/Third_Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/subdir.mk
@@ -0,0 +1,25 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Third_Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/port.c 
+
+OBJS += \
+./Third_Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/port.o 
+
+C_DEPS += \
+./Third_Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/port.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Third_Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/%.o: ../Third_Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo $(PWD)
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -DSTM32 -DSTM32F4 -DSTM32F446RETx -DNUCLEO_F446RE -DDEBUG -DSTM32F446xx -DUSE_STDPERIPH_DRIVER -I"/Users/sanujkul/Documents/Udemy/STM-RTOS-Workspace/freeRTOS_Mutex_Mutual-Ex/StdPeriph_Driver/inc" -I"/Users/sanujkul/Documents/Udemy/STM-RTOS-Workspace/freeRTOS_Mutex_Mutual-Ex/Config" -I"/Users/sanujkul/Documents/Udemy/STM-RTOS-Workspace/freeRTOS_Mutex_Mutual-Ex/Third_Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F" -I"/Users/sanujkul/Documents/Udemy/STM-RTOS-Workspace/freeRTOS_Mutex_Mutual-Ex/Third_Party/FreeRTOS/org/Source/include" -I"/Users/sanujkul/Documents/Udemy/STM-RTOS-Workspace/freeRTOS_Mutex_Mutual-Ex/inc" -I"/Users/sanujkul/Documents/Udemy/STM-RTOS-Workspace/freeRTOS_Mutex_Mutual-Ex/CMSIS/device" -I"/Users/sanujkul/Documents/Udemy/STM-RTOS-Workspace/freeRTOS_Mutex_Mutual-Ex/CMSIS/core" -O0 -g3 -Wall -fmessage-length=0 -ffunction-sections -c -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ freeRTOS_Mutex_Mutual-Ex/Debug/Third_Party/FreeRTOS/org/Source/queue.d
@@ -0,0 +1,35 @@
+Third_Party/FreeRTOS/org/Source/queue.o: \
+ ../Third_Party/FreeRTOS/org/Source/queue.c \
+ /Users/sanujkul/Documents/Udemy/STM-RTOS-Workspace/freeRTOS_Mutex_Mutual-Ex/Third_Party/FreeRTOS/org/Source/include/FreeRTOS.h \
+ /Users/sanujkul/Documents/Udemy/STM-RTOS-Workspace/freeRTOS_Mutex_Mutual-Ex/Config/FreeRTOSConfig.h \
+ /Users/sanujkul/Documents/Udemy/STM-RTOS-Workspace/freeRTOS_Mutex_Mutual-Ex/Third_Party/FreeRTOS/org/Source/include/projdefs.h \
+ /Users/sanujkul/Documents/Udemy/STM-RTOS-Workspace/freeRTOS_Mutex_Mutual-Ex/Third_Party/FreeRTOS/org/Source/include/portable.h \
+ /Users/sanujkul/Documents/Udemy/STM-RTOS-Workspace/freeRTOS_Mutex_Mutual-Ex/Third_Party/FreeRTOS/org/Source/include/deprecated_definitions.h \
+ /Users/sanujkul/Documents/Udemy/STM-RTOS-Workspace/freeRTOS_Mutex_Mutual-Ex/Third_Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h \
+ /Users/sanujkul/Documents/Udemy/STM-RTOS-Workspace/freeRTOS_Mutex_Mutual-Ex/Third_Party/FreeRTOS/org/Source/include/mpu_wrappers.h \
+ /Users/sanujkul/Documents/Udemy/STM-RTOS-Workspace/freeRTOS_Mutex_Mutual-Ex/Third_Party/FreeRTOS/org/Source/include/task.h \
+ /Users/sanujkul/Documents/Udemy/STM-RTOS-Workspace/freeRTOS_Mutex_Mutual-Ex/Third_Party/FreeRTOS/org/Source/include/list.h \
+ /Users/sanujkul/Documents/Udemy/STM-RTOS-Workspace/freeRTOS_Mutex_Mutual-Ex/Third_Party/FreeRTOS/org/Source/include/queue.h \
+ /Users/sanujkul/Documents/Udemy/STM-RTOS-Workspace/freeRTOS_Mutex_Mutual-Ex/Third_Party/FreeRTOS/org/Source/include/task.h
+
+/Users/sanujkul/Documents/Udemy/STM-RTOS-Workspace/freeRTOS_Mutex_Mutual-Ex/Third_Party/FreeRTOS/org/Source/include/FreeRTOS.h:
+
+/Users/sanujkul/Documents/Udemy/STM-RTOS-Workspace/freeRTOS_Mutex_Mutual-Ex/Config/FreeRTOSConfig.h:
+
+/Users/sanujkul/Documents/Udemy/STM-RTOS-Workspace/freeRTOS_Mutex_Mutual-Ex/Third_Party/FreeRTOS/org/Source/include/projdefs.h:
+
+/Users/sanujkul/Documents/Udemy/STM-RTOS-Workspace/freeRTOS_Mutex_Mutual-Ex/Third_Party/FreeRTOS/org/Source/include/portable.h:
+
+/Users/sanujkul/Documents/Udemy/STM-RTOS-Workspace/freeRTOS_Mutex_Mutual-Ex/Third_Party/FreeRTOS/org/Source/include/deprecated_definitions.h:
+
+/Users/sanujkul/Documents/Udemy/STM-RTOS-Workspace/freeRTOS_Mutex_Mutual-Ex/Third_Party/FreeRTOS/org/Source/portable/GCC/ARM_CM4F/portmacro.h:
+
+/Users/sanujkul/Documents/Udemy/STM-RTOS-Workspace/freeRTOS_Mutex_Mutual-Ex/Third_Party/FreeRTOS/org/Source/include/mpu_wrappers.h:
+
+/Users/sanujkul/Documents/Udemy/STM-RTOS-Workspace/freeRTOS_Mutex_Mutual-Ex/Third_Party/FreeRTOS/org/Source/include/task.h:
+
+/Users/sanujkul/Documents/Udemy/STM-RTOS-Workspace/freeRTOS_Mutex_Mutual-Ex/Third_Party/FreeRTOS/org/Source/include/list.h:
+
+/Users/sanujkul/Documents/Udemy/STM-RTOS-Workspace/freeRTOS_Mutex_Mutual-Ex/Third_Party/FreeRTOS/org/Source/include/queue.h:
+
+/Users/sanujkul/Documents/Udemy/STM-RTOS-Workspace/freeRTOS_Mutex_Mutual-Ex/Third_Party/FreeRTOS/org/Source/include/task.h:
